b9e7cdcec34451b768f9b700b7d933746c788c7b
MOHAMMED NASSER <FOREVERMAN.FOREVERMAN@YAHOO.COM>
2021-11-27 18:38:13 +0200

    track specific color for Raspberry pi 4

--- TrackColorWithMotor.py
+++ TrackColorWithMotor.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 from adafruit_servokit import ServoKit
 import Adafruit_PCA9685
+
 pwm = Adafruit_PCA9685.PCA9685()
 kit = ServoKit(channels=16)
 # adjusting motor parameters
@@ -11,7 +12,7 @@ kit = ServoKit(channels=16)
 
 # Set frequency to 60hz, good for servos.
 pwm.set_pwm_freq(60)
-# function to compine shown windows
+# function to combine shown windows
 '''def stackImages(scale,imgArray):
    rows = len(imgArray) #setting the rows
    cols = len(imgArray[0]) #setting the colums 
@@ -44,82 +45,77 @@ pwm.set_pwm_freq(60)
    return ver
 '''
 
-cap = cv2.VideoCapture(0) #initiate camera capture 
+cap = cv2.VideoCapture(0)  # initiate camera capture
 
-
-x_medium = 320 #detected line position
-position = 320 # variable to set initial position in middle of screen range
+x_medium = 320  # detected line position
+position = 320  # variable to set initial position in middle of screen range
 # pwm.set_pwm(0, 0, position)  # setting initial position in middle of screen range
 while True:
-    _, frame = cap.read() #making copy of each frame
-    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) #convert frame from RGB to HSV
+    _, frame = cap.read()  # making copy of each frame
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  # convert frame from RGB to HSV
 
     # selecting red color 
     low_red = np.array([62, 119, 159])
     high_red = np.array([145, 183, 255])
-    #selecting orange color 
+    # selecting orange color
     low_orange = np.array([0, 174, 152])
     high_orange = np.array([179, 255, 255])
-    #creating masks to isolate the prefered color 
-    orange_mask = cv2.inRange(hsv_frame, low_red, high_red) #selected color is orange
+    # creating masks to isolate the prefered color
+    orange_mask = cv2.inRange(hsv_frame, low_red, high_red)  # selected color is orange
     contours, _ = cv2.findContours(orange_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
     orange = cv2.bitwise_and(frame, frame, mask=orange_mask)
 
-    #low = np.array([0, 42, 0])
-    #high = np.array([179, 255, 255])
-    #mask = cv2.inRange(hsv_frame, low, high)
-    #R = cv2.bitwise_and(frame, frame, mask=mask)
-     
-    #draw a rectangular around the detected object  
+    # low = np.array([0, 42, 0])
+    # high = np.array([179, 255, 255])
+    # mask = cv2.inRange(hsv_frame, low, high)
+    # R = cv2.bitwise_and(frame, frame, mask=mask)
+
+    # draw a rectangular around the detected object
     for cnt in contours:
-       (x, y, w, h) = cv2.boundingRect(cnt)
-       cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)  # to drow rectangel
-       x_medium = int((x + x + w) / 2) # determine x center of rectangular
+        (x, y, w, h) = cv2.boundingRect(cnt)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)  # to drow rectangel
+        x_medium = int((x + x + w) / 2)  # determine x center of rectangular
 
-       break # to exit the loop when the biggest value is detected
+        break  # to exit the loop when the biggest value is detected
 
-    cv2.line(frame, (x_medium, 0), (x_medium, 480), (0, 255, 0), 2) #create line at the center of rectangular
-    cv2.line(frame, (320, 0), (320, 480), (0, 255, 0), 2)#create line at the center of frame
-    
-    y=320-x_medium # destance from the center of rectangular and the center of frame 
-    print("y = " , 320-x_medium , "pos = " ,position) #testing print 
+    cv2.line(frame, (x_medium, 0), (x_medium, 480), (0, 255, 0), 2)  # create line at the center of rectangular
+    cv2.line(frame, (320, 0), (320, 480), (0, 255, 0), 2)  # create line at the center of frame
 
-    #cv2.imshow("red", orange)
-    cv2.imshow("hopa", frame) # show the frame 
-    #imgStack = stackImages(0.7,([orange,frame])) #calling the imgstack function to compine the mask and the frame
-    #cv2.imshow("imgStack", imgStack) # show both the frame and the mask
+    y = 320 - x_medium  # distance from the center of rectangular and the center of frame
+    print("y = ", 320 - x_medium, "pos = ", position)  # testing print
+
+    # cv2.imshow("red", orange)
+    cv2.imshow("hopa", frame)  # show the frame
+    # imgStack = stackImages(0.7,([orange,frame])) #calling the imgstack function to compine the mask and the frame
+    # cv2.imshow("imgStack", imgStack) # show both the frame and the mask
 
     key = cv2.waitKey(1)
-    
-    if key == 27: # closing all windows when esc is pressed
+
+    if key == 27:  # closing all windows when esc is pressed
         break
 
-
     # Move servo motor
-    if x_medium < 320 - 30: # moving servo the the left + permittivity 30
+    if x_medium < 320 - 30:  # moving servo the the left + permittivity 30
         position = position + 2
-        if position > 620: # checking if the maximum limit (angle = 180 ) of the servo is reached
-            position=620
-           # kit.servo[11].angle = 105
-            #time.sleep(.1)
-            #kit.servo[11].angle = 90
-    elif x_medium > 320 + 30: # moving servo the the right + permittivity 30
+        if position > 620:  # checking if the maximum limit (angle = 180 ) of the servo is reached
+            position = 620
+        # kit.servo[11].angle = 105
+        # time.sleep(.1)
+        # kit.servo[11].angle = 90
+    elif x_medium > 320 + 30:  # moving servo the the right + permittivity 30
         position = position - 2
-        if position < 100: #checking if the minimum limit (angle = 0 ) of the servo is reached
-            position=100
-           # print("111")
-            #kit.servo[11].angle = 85
-            #print("222")
-            #time.sleep(.1)
-            #print("333")
-            #kit.servo[11].angle = 90
-            #print("444")
-        
-       
+        if position < 100:  # checking if the minimum limit (angle = 0 ) of the servo is reached
+            position = 100
+        # print("111")
+        # kit.servo[11].angle = 85
+        # print("222")
+        # time.sleep(.1)
+        # print("333")
+        # kit.servo[11].angle = 90
+        # print("444")
+
     pwm.set_pwm(0, 0, position)  # sending position to the
 
-        
-cap.release() #stop racording frame
-cv2.destroyAllWindows() #close all windows 
-
+cap.release()  # stop racording frame
+cv2.destroyAllWindows()  # close all windows
